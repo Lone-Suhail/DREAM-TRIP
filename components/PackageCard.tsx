ffb9617ce74e21d96ec0872960a3bff97c34bad1
Lone-Suhail <lonesuhail02@gmail.com>
2025-12-17 14:45:21 +0530

    updated packages

--- components/PackageCard.tsx
+++ components/PackageCard.tsx
@@ -106,7 +106,7 @@ const PackageCard: React.FC<PackageCardProps> = ({ data }) => {
               </div>
 
               {/* --- LINK FIXED HERE --- */}
-              <Link href={`/packages/${data.id}`}>
+              <Link href={`/packages=${data.id}`}>
                 <button className="bg-[#1E3A8A] hover:bg-blue-900 text-white p-3 rounded-xl shadow-lg transition-all hover:scale-105 active:scale-95 flex items-center justify-center">
                    <ArrowRight size={20} />
                 </button>
